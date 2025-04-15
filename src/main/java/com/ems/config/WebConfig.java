493a84b1cb41b565af374a5a4f4e1f116450dd0d
Asif Khan <asifkl786@gmail.com>
2025-04-15 11:03:22 +0530

    modifies webconfig

--- src/main/java/com/ems/config/WebConfig.java
+++ src/main/java/com/ems/config/WebConfig.java
@@ -27,7 +27,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "https://candid-cuchufli-484d53.netlify.app",
                         "https://ems-curd-app.netlify.app", // ✅ your frontend URL
                         "http://localhost:5173" // ✅ for local dev if needed
                     )
